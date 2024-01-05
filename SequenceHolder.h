5190c60eba5df22d414b2d4c8eb9a319e744ee84
james.alexander.mcmanus@gmail.com <jamesmcmanus@bi1jms-air.home>
2024-01-05 16:22:30 +0000

    lib changes

--- SequenceHolder.h
+++ SequenceHolder.h
@@ -8,12 +8,11 @@
 class SequenceHolder: public MessageParser {
 	public:
 		int max_seq_len;
-		//std::queue<int> sequence;
 		boost::circular_buffer<int> sequence;
 		SequenceHolder(int seqlen, int socket, int buf_len);
 		int checkCollatz();
 		int addFromMessage();
-		int front();
+		int front(); // get from the front
 };
 
 #endif
--- testQT2/testQT2.pro
+++ testQT2/testQT2.pro
@@ -7,6 +7,8 @@ CONFIG += c++17
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+LIBS +=-lboost_thread
+
 SOURCES += \
     ../qcustomplot/qcustomplot.cpp \
     main.cpp \
