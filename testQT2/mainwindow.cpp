eef0e70aff0d70905bbc3802c43c55eda02d29ac
james.alexander.mcmanus@gmail.com <jamesmcmanus@bi1jms-air.home>
2024-01-05 14:18:26 +0000

    basicGUI

---
+++ testQT2/mainwindow.cpp
@@ -0,0 +1,41 @@
+#include "mainwindow.h"
+#include "qcustomplot.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+};
+
+void MainWindow::generateData(){
+    // generate some data:
+    QVector<double> x(101), y(101); // initialize with entries 0..100
+    for (int i=0; i<101; ++i)
+    {
+        x[i] = i/50.0 - 1; // x goes from -1 to 1
+        y[i] = x[i]*x[i]; // let's plot a quadratic function
+    };
+    // create graph and assign data to it:
+    ui->graph->addGraph();
+    ui->graph->graph(0)->setData(x,y);//(0)->setData(x, y);
+    // give the axes some labels:
+    ui->graph->xAxis->setLabel("x");
+    ui->graph->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    ui->graph->xAxis->setRange(-1, 1);
+    ui->graph->yAxis->setRange(0, 1);
+    ui->graph->replot();
+}
+
+void MainWindow::on_updateGraph_clicked()
+{
+    this->generateData();
+}
+
